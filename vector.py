a9350535a9fb2799357dae93e5995be2901cd8bf
Cameron White <cameronw@lavabit.com>
2013-08-22 10:18:58 -0700

    Add comments, doctests and fixed errors in vector module

--- vector.py
+++ vector.py
@@ -28,80 +28,191 @@
 # -*- coding: utf-8 -*-
 from numbers import Number
 from functools import reduce
+from math import sqrt
 
 class Vector(object):
-    def __init__(self, element):
-        if isinstance(element, Vector):
-            self._elements = element.elements
-        elif isinstance(element, list):
-            self._elements = element
-        elif isinstance(element, Number):
-            self._elements = [0]*element
+    """ Mathematical Vector """
+
+    def __init__(self, value):
+        """ Construct a Vector
+
+        If value is a number the vector becomes a vector all 0 of
+        that length. If the value is a list the vector will 
+        contain the elements of that length with out any copying.
+        Any other type for value will be converted to list if 
+        possible, this will probably result in copying of data.
+        Also if value is a Vector then the constructor copies.
+
+        >>> Vector([1, 2, 3, 4, 5])
+        Vector(1, 2, 3, 4, 5)
+        >>> Vector([0.1, 0.2, 0.3, 0.4, 0.5])
+        Vector(0.1, 0.2, 0.3, 0.4, 0.5)
+        >>> Vector(5)
+        Vector(0, 0, 0, 0, 0)
+
+        Args:
+            value: list, number, or something that can be converted 
+                   to a list.
+        """
+        if isinstance(value, Vector):
+            self._elements = value._elements
+        elif isinstance(value, list):
+            self._elements = value
+        elif isinstance(value, Number):
+            self._elements = [0]*value
         else:
-            self._elements = list(element)
+            self._elements = list(value)
 
     def __repr__(self):
+        """ Return the representation of the Vector
+
+        >>> Vector([1,2,3])
+        Vector(1, 2, 3)
+        """
         return 'Vector' + '('\
                 + str(reduce(lambda x, y: str(x) + ', ' + str(y), self._elements))\
                 + ')'
 
     def __str__(self):
+        """ Return the string of the Vector
+
+        >>> print(Vector([1,2,3]))
+        [1, 2, 3]
+        """
         return '[' \
                 + str(reduce(lambda x, y: str(x) + ', ' + str(y), self._elements))\
                 + ']'
 
     def __add__(self, other):
-        self._lengthCheck(other)
-        new = Vector(len(self))
-        for x,y,z in zip(new, self, other):
-            x = y + z
-        return new
+        """ Add this vector to the other vector in a new vector
+
+        >>> v1 = Vector([1, 2, 3, 4, 5])
+        >>> v2 = Vector([0.1, 0.2, 0.3, 0.4, 0.5])
+        >>> v1 + v2
+        Vector(1.1, 2.2, 3.3, 4.4, 5.5)
+
+        Args:
+            other: The other vector
+
+        Returns:
+            The new vector.
+        """
+        try:
+            return Vector(map(lambda a, b: a + b, self, other))
+        except TypeError:
+            raise VectorLengthError
 
     def __iadd__(self, other):
-        self._lengthCheck(other)
-        for x,y in zip(self, other):
-            x += y
-        return self
+        """ Add this vector to the other vector in place.
+
+        >>> v1 = Vector([1, 2, 3, 4, 5])
+        >>> v2 = Vector([0.1, 0.2, 0.3, 0.4, 0.5])
+        >>> v1 += v2
+        >>> v1
+        Vector(1.1, 2.2, 3.3, 4.4, 5.5)
+
+        Args:
+            other: The other vector
+        """
+        return self.copy(self + other)
 
     def __sub__(self, other):
-        self._lengthCheck(len(other))
-        new = Vector(len(self))
-        for x,y,z in zip(new, self, other):
-            x = y - z
-        return new
+        """ Subtract the other vector from this vector in a new vector.
+
+        >>> v1 = Vector([1, 2, 3, 4, 5])
+        >>> v2 = Vector([0.1, 0.2, 0.3, 0.4, 0.5])
+        >>> v1 - v2
+        Vector(0.9, 1.8, 2.7, 3.6, 4.5)
+
+        Args:
+            other: The other vector
+
+        Returns:
+            The new vector.
+        """
+        try:
+            return Vector(map(lambda a, b: a - b, self, other))
+        except TypeError:
+            raise VectorLengthError
 
     def __isub__(self, other):
-        self._lengthCheck(len(other))
-        for x,y in zip(self, other):
-            x -= y
-        return self
+        """ Subtract the other vector from this vector in a new vector.
+
+        >>> v1 = Vector([1, 2, 3, 4, 5])
+        >>> v2 = Vector([0.1, 0.2, 0.3, 0.4, 0.5])
+        >>> v1 - v2
+        Vector(0.9, 1.8, 2.7, 3.6, 4.5)
+
+        Args:
+            other: The other vector
+        """
+        return self.copy(self - other)
 
     def __iter__(self):
         for element in self._elements:
             yield element
 
     def __setitem__(self, index, value):
+        """ Set the element to the value at the index.
+
+        >>> v1 = Vector([0, 0, 0])
+        >>> v1[0] = 1
+        """
         self._elements[index] = value
 
     def __getitem__(self, index):
+        """ Get the element at the index.
+
+        >>> v1 = Vector([1, 2, 3])
+        >>> v1[0]
+        1
+        >>> v1[1]
+        2
+        """
         return self._elements[index]
 
     def __len__(self):
+        """ Return the length of the vector.
+
+        >>> len(Vector([1, 2, 3]))
+        3
+        """
         return len(self._elements)
 
     def __abs__(self):
-         return self.dot(self)
+        """ Return the magnitude of the Vector.
+
+        >>> abs(Vector([1, 2, 3]))
+        3.7416573867739413
+        """
+        return sqrt(self.dot(self))
+
+    def copy(self, other):
+        """ Copy the elements of the other vector
+
+        >>> v1 = Vector([1, 2, 3])
+        >>> v2 = Vector(0)
+        >>> v2 = Vector(v1)
+        >>> v2._elements is v1._elements
+        True
+        """
+        self._elements = other._elements
+        return self
 
     def dot(self, other):
-        self._lengthCheck(len(other))
-        return reduce(lambda x, y: x+y, map(lambda x, y: x*y, self, other))
+        """ Return the dot product of the two vectors
 
-    def _lengthCheck(self, other):
-        if len(self) != len(other):
-            raise VectorLengthError
+        >>> Vector([1,2]).dot(Vector([2, 3]))
+        8
+        """
+        return reduce(lambda x, y: x+y, map(lambda x, y: x*y, self, other))
 
 class VectorLengthError(Exception):
     def __init__(self, value):
         self.parameter = value
     def __str__(self):
         return repr(self.parameter)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
