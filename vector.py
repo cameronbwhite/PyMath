df37bd7dc6a3c9c75606527d31b958965714b079
Cameron White <cameronw@lavabit.com>
2013-08-22 00:31:26 -0700

    Updated vector module/class

--- vector.py
+++ vector.py
@@ -1,118 +1,78 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from numbers import Number
+from functools import reduce
 
 class Vector(object):
     def __init__(self, element):
         if isinstance(element, Vector):
-            self.elements = element.elements
+            self._elements = element.elements
+        elif isinstance(element, list):
+            self._elements = element
+        elif isinstance(element, Number):
+            self._elements = [0]*element
         else:
-            try:
-                self.elements = list(element)
-            except:
-                self.elements = [element]
+            self._elements = list(element)
+
     def __repr__(self):
         return 'Vector' + '('\
-                + str(reduce(lambda x, y: str(x) + ', ' + str(y), self.elements))\
+                + str(reduce(lambda x, y: str(x) + ', ' + str(y), self._elements))\
                 + ')'
+
     def __str__(self):
         return '[' \
-                + str(reduce(lambda x, y: str(x) + ', ' + str(y), self.elements))\
+                + str(reduce(lambda x, y: str(x) + ', ' + str(y), self._elements))\
                 + ']'
+
     def __add__(self, other):
-        self._vectorTypeCheck(type(other))
-        self._lengthCheck(len(other))
-        length = len(self)
-        new_vector = self._new_vector(type(self), length)
-        for i in range(length):
-             new_vector[i] = self[i] + other[i]
-        return new_vector
+        self._lengthCheck(other)
+        new = Vector(len(self))
+        for x,y,z in zip(new, self, other):
+            x = y + z
+        return new
+
     def __iadd__(self, other):
-        self._vectorTypeCheck(type(other))
-        self._lengthCheck(len(other))
-        length = len(self)
-        for i in range(length):
-             self[i] += other[i]
+        self._lengthCheck(other)
+        for x,y in zip(self, other):
+            x += y
         return self
+
     def __sub__(self, other):
-        self._vectorTypeCheck(type(other))
         self._lengthCheck(len(other))
-        length = len(self)
-        new_vector = self._new_vector(type(self), length)
-        for i in range(length):
-             new_vector[i] = self[i] - other[i]
-        return new_vector
+        new = Vector(len(self))
+        for x,y,z in zip(new, self, other):
+            x = y - z
+        return new
+
     def __isub__(self, other):
-        self._vectorTypeCheck(type(other))
         self._lengthCheck(len(other))
-        for i in range(lenght):
-            self[i] -= other[i]
+        for x,y in zip(self, other):
+            x -= y
         return self
+
     def __iter__(self):
-        for element in self.elements:
+        for element in self._elements:
             yield element
+
     def __setitem__(self, index, value):
-        self._indexCheck(index)
-        self.elements[index] = value
+        self._elements[index] = value
+
     def __getitem__(self, index):
-        self._indexCheck(index)
-        return self.elements[index]
+        return self._elements[index]
+
     def __len__(self):
-        return len(self.elements)
+        return len(self._elements)
+
     def __abs__(self):
          return self.dot(self)
+
     def dot(self, other):
-        self._vectorTypeCheck(type(other))
-        assert type(other) == Vector
-        assert len(self) == len(other)
+        self._lengthCheck(len(other))
         return reduce(lambda x, y: x+y, map(lambda x, y: x*y, self, other))
-    def _indexCheck(self, index):
-        if index < 0 or index >= len(self):
-            raise IndexError
-    def _new_vector(self, vector_type, length, element=0):
-        if vector_type is RowVector or vector_type is ColumnVector:
-            return vector_type([element]*length)
-        else:
-            raise TypeError
-    def _lengthCheck(self, length1, length2=None):
-        if length2 is None:
-            length2 = len(self)
-        if type(length1) is type(length2) is int: 
-            if length1 == length2:
-                return True
-            else:
-                raise VectorLengthError
-        else:
-            raise TypeError
-    def _vectorTypeCheck(self, vectorType1, vectorType2=None):
-        if vectorType2 is None:
-            vectorType2 = type(self)
-        if vectorType1 is vectorType2 is RowVector:
-                return True
-        elif vectorType1 is vectorType2 is ColumnVector:
-                return True
-        else:
-            raise TypeError
 
-class RowVector(Vector):
-    def __repr__(self):
-        return 'RowVector' + '('\
-                + str(reduce(lambda x, y: str(x) + ', ' + str(y), self.elements)) \
-                + ')'
-    def __str__(self):
-        return '[' \
-                + str(reduce(lambda x, y: str(x) + ', ' + str(y), self.elements))\
-                + ']'
-
-class ColumnVector(Vector):
-    def __repr__(self):
-        return 'ColumnVector' + '('\
-                + str(reduce(lambda x, y: str(x) + ', ' + str(y), self.elements))\
-                + ')'
-    def __str__(self):
-        return '[' \
-                + str(reduce(lambda x, y:  str(x) + ',\n' + ' ' + str(y), self.elements))\
-                + ']'
+    def _lengthCheck(self, other):
+        if len(self) != len(other):
+            raise VectorLengthError
 
 class VectorLengthError(Exception):
     def __init__(self, value):
